68edb3fe3d4823d6a85b8fbe8ac8dee08dae47e4
brandonkhoza <8brandonkhoza@gmail.com>
2023-10-24 13:31:11 +0200

    Basic Form Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Form, Field } from 'react-final-form';
+
+//const sleep = ms = new Promise(resolve => setTimeout(resolve, ms));
+const showResults = values => {
+
+  //await sleep(500);
+  alert(JSON.stringify(values, undefined, 2));
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Final Form Basics</h1>
+      <Form onSubmit={showResults}>
+        {/* handles form states */}
+        {({ handleSubmit, values, submitting }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>First Name</label>
+              <Field
+                name="firstname"
+                component="input"
+                placeholder="First Name"
+              ></Field>
+            </div>
+            <div>
+              <label>Last Name</label>
+              <Field
+                name="lastname"
+                component="input"
+                placeholder="Last Name"
+              ></Field>
+            </div>
+            <div>
+              <label>Email</label>
+              <Field
+                name="email"
+                component="input"
+                placeholder="Email"
+              ></Field>
+            </div>
+            <button type='submit' disabled={submitting}>Submit</button>
+            <h2>Real Time View Of Values</h2>
+            <pre>
+              {JSON.stringify(values, undefined, 2)}
+            </pre>
+          </form>
+        )}
+
+      </Form>
     </div>
   );
 }
